ac764f4b43c4b573ad222ec0ef57cf3d8f3c8655
trgrote <grotese@gmail.com>
2025-02-08 22:03:27 -0600

    Add Abstract EventEnumEditor

---
+++ Editor/EventEnumEditor.cs
@@ -0,0 +1,27 @@
+using UnityEditor;
+using System.Reflection;
+using System;
+using System.Linq;
+
+namespace rho
+{
+    public class EventEnumEditor<T> : EventEditor<T> where T : System.Enum
+    {
+        protected T _testParam;
+
+        protected bool _isFlag;
+
+		protected virtual void OnEnable()
+		{
+			_isFlag = typeof(T).GetCustomAttributes<FlagsAttribute>().Any();
+		}
+
+		protected override T GetTestParam()
+		{
+			return _isFlag ?
+				_testParam = (T) EditorGUILayout.EnumFlagsField("Value", _testParam) :
+				_testParam = (T) EditorGUILayout.EnumPopup("Value", _testParam)
+			;
+		}
+    }
+}
