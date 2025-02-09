cc125bb5f7609041a3ab64572166945f94fdc31f
trgrote <grotese@gmail.com>
2025-02-09 14:38:04 -0600

    Update Wizard to autopopulate editor script based off type

--- Editor/GenerateNewEventTypeWizard.cs
+++ Editor/GenerateNewEventTypeWizard.cs
@@ -1,7 +1,7 @@
+using System;
 using System.IO;
 using System.Linq;
 using UnityEditor;
-using UnityEngine;
 
 namespace rho
 {
@@ -13,56 +13,123 @@ namespace rho
         [MenuItem("Assets/Create/Rho/Create New Event<T> Type")]
         static void Init() => DisplayWizard<GenerateNewEventTypeWizard>("Create New Event<T>");
 
+        #region SerializedFields
+        
+        public string _typeName;
+
+        public bool _createEditorScript;
+
+        #endregion
+
         #region ScriptableWizard Methods
 
         void OnWizardUpdate()
         {
             helpString = "Creates a new Event<T> of type";
-            if (typeName == null || typeName.Length == 0)
+            errorString = "";
+            isValid = true;
+
+            if (_typeName == null || _typeName.Length == 0)
             {
                 errorString = "Please Provide Type";
                 isValid = false;
+                return;
             }
-            else if(PathForEventAlreadyExists())
+            
+            // Try to find type
+            var types = TypeSearcher.SearchForType(_typeName);
+
+            if (types.Count() == 1)
             {
-                errorString = GetFullEventPathName() + " Already Exists";
+                var type = types.First();
+                helpString = $"Creates a new Event<{type.FullName}> type";
+            }
+            else if (types.Count() > 1)
+            {
+                errorString = $"Found {types.Count()} Types: ";
+                if (types.Count() < 10)
+                {
+                    errorString += string.Join(',', types.Select(t => t.FullName));
+                }
                 isValid = false;
-            }
-            else
-            {
-                errorString = "";
-                isValid = true;
             }
         }
 
         void OnWizardCreate()
         {
+            var types = TypeSearcher.SearchForType(_typeName);
+
+            if (types.Count() == 1)
+            {
+                var type = types.First();
+                CreateEventScripts(type, _createEditorScript);
+            }
+            else
+            {
+                UnityEngine.Debug.LogError("Attempting to create event scripts of unknown type");
+            }
+        }
+
+        void CreateEventScripts(Type type, bool createEditorScript)
+        {
+            var fullTypeName = type.FullName;
+            var prettyTypeName = GetPrettyTypeName(type);
+            var eventClassName = $"Event{prettyTypeName}";
+
             // Create new Folder
-            AssetDatabase.CreateFolder(GetEventAssetPath(), GetEventClassName());
+            var folderGUID = AssetDatabase.CreateFolder(GetSelectedAssetFolder(), eventClassName);
+            var folderName = AssetDatabase.GUIDToAssetPath(folderGUID);
 
             // Create new Event Script, inside folder
-            CreateNewScript(GetFullEventScriptName(), GetEventScriptContents());
+            var eventScriptName = $"{folderName}/{eventClassName}.cs";
+            CreateNewScript(eventScriptName, GetEventScriptContents(
+                eventClassName: eventClassName, 
+                fullTypeName: fullTypeName
+            ));
 
             // Create new Event Listener Script, inside folder
-            CreateNewScript(GetFullEventListenerScriptName(), GetEventListenerScriptContents());
+            var eventListenerClassName = $"{eventClassName}Listener";
+            var eventListenerScriptName = $"{folderName}/{eventListenerClassName}.cs";
+            CreateNewScript(eventListenerScriptName, GetEventListenerScriptContents(
+                listenerClassName: eventListenerClassName, 
+                fullTypeName: fullTypeName
+            ));
 
             // Create Editor Script, if they want
             if (createEditorScript)
             {
-                AssetDatabase.CreateFolder(GetFullEventPathName(), "Editor");
-                CreateNewScript(GetFullEventEditorScriptName(), GetEditorScriptContents());
+                var editorFolderGUID = AssetDatabase.CreateFolder(folderName, "Editor");
+                var editorFolderName = AssetDatabase.GUIDToAssetPath(editorFolderGUID);
+                
+                var eventEditorClassName = $"{eventClassName}Editor";
+                var eventEditorScriptName = $"{editorFolderName}/{eventEditorClassName}.cs";
+
+                CreateNewScript(eventEditorScriptName, GetEditorScriptContents(
+                    editorClassName: eventEditorClassName, 
+                    eventClassName: eventClassName,
+                    fullTypeName: fullTypeName,
+                    type: type
+                ));
             }
 
             AssetDatabase.Refresh();
         }
 
+        private string GetPrettyTypeName(Type type)
+        {
+            // Check if the type is a built in type
+            if (TypeSearcher.TryGetBuiltInType(type, out var builtInName))
+            {
+                // if so, use the built in name
+                return Capitalize(builtInName);
+            }
+
+            return type.Name;
+        }
+
         #endregion
 
         #region String Methods
-        
-        public string typeName;
-
-        public bool createEditorScript;
 
         static string Capitalize(string str)
         {
@@ -73,10 +140,10 @@ namespace rho
             return str;
         }
 
-        private static string GetSelectedFolder()
+        private static string GetSelectedAssetFolder()
         {
             var targetFolder = "Assets";
-            var selectedAsset = Selection.GetFiltered<Object>(SelectionMode.Assets).FirstOrDefault();
+            var selectedAsset = Selection.GetFiltered<UnityEngine.Object>(SelectionMode.Assets).FirstOrDefault();
             if (selectedAsset)
             {
                 var assetPath = AssetDatabase.GetAssetPath(selectedAsset);
@@ -92,65 +159,59 @@ namespace rho
 
             return targetFolder;
         }
-        
-        string GetEventAssetPath() => GetSelectedFolder();
-        string GetEventFolderName() => $"Event{Capitalize(typeName)}";
-        string GetFullEventPathName() => $"{GetEventAssetPath()}/{GetEventFolderName()}";
-
-        string GetEventClassName() => $"Event{Capitalize(typeName)}";
-        string GetEventListenerClassName() => $"EventListener{Capitalize(typeName)}";
-        string GetEventEditorClassName() => $"Event{Capitalize(typeName)}Editor";
-
-        string GetFullEventScriptName() => $"{GetFullEventPathName()}/{GetEventClassName()}.cs";
-        string GetFullEventListenerScriptName() => $"{GetFullEventPathName()}/{GetEventListenerClassName()}.cs";
-        string GetFullEventEditorScriptName() => $"{GetFullEventPathName()}/Editor/{GetEventEditorClassName()}.cs";
-        
-        bool PathForEventAlreadyExists() => AssetDatabase.IsValidFolder(GetFullEventPathName());
 
         #endregion
 
         #region Script Writing
+
         void CreateNewScript(string fullScriptName, string contents)
         {
             using StreamWriter sw = File.CreateText(fullScriptName);
             sw.WriteLine(contents);
         }
 
-        string GetEventScriptContents()
+        string GetEventScriptContents(string eventClassName, string fullTypeName)
         {
-            return $@"using UnityEngine;
-
-[CreateAssetMenu(fileName = ""{GetEventClassName()}"", menuName = ""Events/Event<{typeName}>"")]
-public class {GetEventClassName()} : rho.Event<{typeName}>
+            return $@"[UnityEngine.CreateAssetMenu(fileName = ""{eventClassName}"", menuName = ""Events/Event<{fullTypeName}>"")]
+public class {eventClassName} : rho.Event<{fullTypeName}>
 {{
 }}";
         }
 
-        string GetEventListenerScriptContents()
+        string GetEventListenerScriptContents(string listenerClassName, string fullTypeName)
         {
-            return $@"using UnityEngine;
-
-public class {GetEventListenerClassName()} : rho.EventListener<{typeName}>
+            return $@"public class {listenerClassName} : rho.EventListener<{fullTypeName}>
 {{
 }}";
         }
 
-        string GetEditorScriptContents()
+        string GetEditorScriptContents(string editorClassName, string eventClassName, string fullTypeName, Type type)
         {
-            return $@"using UnityEngine;
-using UnityEditor;
+            var baseClassName = "EventEditor";
+            var classBody = $@"{{
+    {fullTypeName} _testParam;
 
-[CustomEditor(typeof({GetEventClassName()}))]
-public class {GetEventEditorClassName()} : rho.EventEditor<{typeName}>
-{{
-    {typeName} _testParam;
-
-    protected override {typeName} GetTestParam()
+    protected override {fullTypeName} GetTestParam()
     {{
         // TODO Implement something like this: return _testParam = EditorGUILayout.Toggle(""Test Parameter"", _testParam);
         throw new System.NotImplementedException();
     }}
 }}";
+
+            if (type.IsSubclassOf(typeof(System.Enum)))
+            {
+                baseClassName = "EventEnumEditor";
+                classBody = "{\n}";
+            }
+            else if (type.IsSubclassOf(typeof(UnityEngine.Object)))
+            {
+                baseClassName = "EventUnityObjectEditor";
+                classBody = "{\n}";
+            }
+
+            return $@"[UnityEditor.CustomEditor(typeof({eventClassName}))]
+public class {editorClassName} : rho.{baseClassName}<{fullTypeName}>
+{classBody}";
         }
         #endregion
     }
