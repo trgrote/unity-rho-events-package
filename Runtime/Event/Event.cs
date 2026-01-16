df2bba3097093e9d9a8ed6f3cbc327f405da4e9e
trgrote <grotese@gmail.com>
2026-01-15 22:22:06 -0600

    Move Parameter-less event into own subfolder

---
+++ Runtime/Event/Event.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace rho
+{
+    [CreateAssetMenu(fileName = "NewEvent", menuName = "Rho/Events/Event")]
+    public class Event : ScriptableObject
+    {
+        List<EventListener> _listeners = new List<EventListener>();
+
+        public void Invoke()
+        {
+            for (var i = _listeners.Count - 1; i >= 0 ; --i)
+            {
+                _listeners[i].OnEventInvoked();
+            }
+        }
+
+        public void Register(EventListener listener)
+        {
+            _listeners.Add(listener);
+        }
+
+        public void Unregister(EventListener listener)
+        {
+            _listeners.Remove(listener);
+        }
+    }
+}
