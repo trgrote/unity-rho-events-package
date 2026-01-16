ed09ddbb440a9a38ac47948412fc349d0299014b
trgrote <grotese@gmail.com>
2026-01-15 22:17:38 -0600

    Update TransformEventEditor to also inherit UnityObjectEventEditor like GameObjectEventEditor does

--- Editor/TransformEventEditor.cs
+++ Editor/TransformEventEditor.cs
@@ -4,13 +4,7 @@ using UnityEditor;
 namespace rho
 {
     [CustomEditor(typeof(TransformEvent))]
-    public class TransformEventEditor : EventEditor<Transform>
+    public class TransformEventEditor : UnityObjectEventEditor<Transform>
     {
-        Transform _testParam;
-
-        protected override Transform GetTestParam()
-        {
-            return _testParam = EditorGUILayout.ObjectField("Test Parameter", _testParam, typeof(Transform), true) as Transform;
-        }
     }
 }
